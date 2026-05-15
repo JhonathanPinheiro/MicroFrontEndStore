e09e8675d9cfad48fc01df40f55747c485dd42c6
Jhonathan Pinheiro <jhonathancpinheiro@gmail.com>
2026-05-15 12:24:46 -0300

    feat: add initial setup for Auth Microfrontend

---
+++ auth-mfe/src/AuthApp.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function AuthApp() {
+
+  return (
+
+    <div style={{border:"2px solid blue", padding:20}}>
+
+      <h2>Auth Microfrontend</h2>
+
+    </div>
+
+  )
+
+}
---
+++ auth-mfe/webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const { ModuleFederationPlugin } = require("webpack").container
+
+module.exports = {
+
+  mode: "development",
+  entry: "./src/index.jsx",
+
+  output: {
+    publicPath: "auto"
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx", ".json"]
+  },
+
+  devServer: {
+    port: 3002,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    }
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react"
+          ]
+        }
+      }
+    ]
+  },
+
+  plugins: [
+
+    new ModuleFederationPlugin({
+
+      name: "auth",
+
+      filename: "remoteEntry.js",
+
+      exposes: {
+        "./AuthApp": "./src/AuthApp.jsx"
+      },
+
+      shared: {
+        react: {
+          singleton: true
+        },
+        "react-dom": {
+          singleton: true
+        }
+      }
+
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+
+  ]
+
+}
